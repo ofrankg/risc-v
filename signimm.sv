a1f48408b5c43ec417b73b4400a2edfa0e1e34f7
ofranco <ofrancog1000@alumno.ipn.mx>
2021-05-04 16:10:57 -0500

    Fix: SignImm for load isntructions

--- signimm.sv
+++ signimm.sv
@@ -10,6 +10,7 @@ module signimm
 
   always_comb begin
     case (instr_in[6:0])
+      op_load : signimm_out = {{20{instr_in[31]}},instr_in[WIDTH-1:20]};
       op_imm  : signimm_out = {{20{instr_in[31]}},instr_in[WIDTH-1:20]};
       op_br   : signimm_out = {{20{instr_in[31]}}, instr_in[7], instr_in[30:25], instr_in[11:8],1'b0};
       op_store: signimm_out = {{20{instr_in[31]}}, instr_in[WIDTH-1:25],instr_in[11:7]};
